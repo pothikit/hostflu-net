efc58d12a60c91111c01ff5304a6e6c1e494cf1e
pothikit <tytanhsot71@gmail.com>
2024-05-06 16:00:42 +0600

    testimonial carousel implemented

--- js/custom.js
+++ js/custom.js
@@ -9,31 +9,54 @@ getYear();
 
 
 // client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
-    navText: [],
+// $(".client_owl-carousel").owlCarousel({
+//     loop: true,
+//     margin: 20,
+//     dots: false,
+//     nav: true,
+//     navText: [],
+//     autoplay: true,
+//     autoplayHoverPause: true,
+//     navText: [
+//         '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+//         '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+//     ],
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 2
+//         },
+//         1000: {
+//             items: 2
+//         }
+//     }
+// });
+
+// Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
     autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
+    smartSpeed: 1000,
+    margin: 25,
+    dots: true,
+    loop: true,
+    center: true,
     responsive: {
         0: {
             items: 1
         },
-        600: {
+        576: {
+            items: 1
+        },
+        768: {
             items: 2
         },
-        1000: {
-            items: 2
+        992: {
+            items: 3
         }
     }
 });
-
 
 
 /** google_map js **/
