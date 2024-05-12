0048bcd4ff1a508c55021d403d547a8b402d356e
pothikit <tytanhsot71@gmail.com>
2024-05-12 16:02:29 +0600

    some changes

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,12 @@ function getYear() {
     var currentYear = currentDate.getFullYear();
     document.querySelector("#displayYear").innerHTML = currentYear;
 }
-
 getYear();
+
+// const navMain = document.querySelector(".navbar");
+// const navItem = navMain.querySelectorAll('.nav-item');
+// console.log(navItem)
+
 
 
 // client section owl carousel
@@ -60,10 +64,10 @@ $(".testimonial-carousel").owlCarousel({
 
 
 /** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(40.712775, -74.005973),
+//         zoom: 18,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
